217bf88da19776c4c52f8675729ac9f44c68e3d9
gsrm1 <gsrm1@hotmail.com>
2024-08-24 20:29:05 +0800

    8/24 fix

--- script/all.js
+++ script/all.js
@@ -22,30 +22,45 @@ $(document).ready(function () {
         'disableScrolling': true
       })
 
-    //   goToTop icon
-    $('.goToTop').on('mouseover', function () {
-        $('.fa-circle-up').attr('style', 'color: red');
+      // goToTop icon
+    function showBtnCondition() {
+      if ($(this).scrollTop() > 600) {
+        $('#goToTop').fadeIn();
+      } 
+      else {
+        $('#goToTop').fadeOut();
+      }
+    }
+    $(window).scroll(showBtnCondition);
+    
+    $('#goToTop').click(function (e) { 
+      $('html,body').animate({scrollTop: 0}, 800);
     });
 
-    $('.goToTop').on('mouseleave', function () {
-        $('.fa-circle-up').attr('style', 'color: #FFD43B');
-    });
-
-    $('.goToTop').click(function (e) { 
-        $('html,body').animate({scrollTop: 0}, 800);
-    });
+    // $('#goToTop').on('mouseover', function () {
+    //   $('.fa-circle-up').attr('style', 'color:red');
+    // });
+    
+    // $('#goToTop').on('mouseleave', function () {
+    //   $('.fa-circle-up').attr('style', 'color:#FFD43B');
+    // });
+    
 
     // menu dropdown-open
     $('.dropdown1').click(function (e) { 
       e.preventDefault();
       $('.dropdown1').toggleClass('active');
+      $('.drop2').slideUp();
       $('.drop1').slideToggle('fast');
+      $('.dropdown2').removeClass('active2');
       
     });
     $('.dropdown2').click(function (e) { 
       e.preventDefault();
       $('.dropdown2').toggleClass('active2');
+      $('.drop1').slideUp();
       $('.drop2').slideToggle('fast');
+      $('.dropdown1').removeClass('active');
       
     });
 });
