10c8eb8a33973e8f6b6725488f33a6438b0fd937
gsrm1 <gsrm1@hotmail.com>
2024-08-27 15:54:07 +0800

    8/27 edit

--- script/all.js
+++ script/all.js
@@ -22,45 +22,67 @@ $(document).ready(function () {
         'disableScrolling': true
       })
 
-      // goToTop icon
-    function showBtnCondition() {
-      if ($(this).scrollTop() > 800) {
-        $('#goToTop').fadeIn();
-      } 
-      else {
-        $('#goToTop').fadeOut();
-      }
-    }
-    $(window).scroll(showBtnCondition);
+    // goToTop icon（使用捲動百分比觸發按鈕）
+  function showBtnCondition(){
+  const scrollTop = $(this).scrollTop();
+  const docHeight = $(document).height();
+  const windowHeight = $(window).height();
+  const scrollPercent = (scrollTop / (docHeight - windowHeight)) * 100;
+  if (scrollPercent > 70){
+    $('#goToTop').fadeIn();
+  }
+  else{
+    $('#goToTop').fadeOut();
+  }
+}
+  $(window).scroll(showBtnCondition);
+
+    // 舊方法（使用滾動px、但遇到高解析度螢幕會失去功能）
+    // function showBtnCondition() {
+    //   if ($(this).scrollTop() > 800) {
+    //     $('#goToTop').fadeIn();
+    //   } 
+    //   else {
+    //     $('#goToTop').fadeOut();
+    //   }
+    // }
+    // $(window).scroll(showBtnCondition);
     
     $('#goToTop').click(function (e) { 
       $('html,body').animate({scrollTop: 0}, 800);
     });
-
-    // $('#goToTop').on('mouseover', function () {
-    //   $('.fa-circle-up').attr('style', 'color:red');
-    // });
     
-    // $('#goToTop').on('mouseleave', function () {
-    //   $('.fa-circle-up').attr('style', 'color:#FFD43B');
-    // });
+    // 應使用如下.css改變樣式而不是用.attr
+    $('#goToTop').on('mouseover', function () {
+      $('.fa-circle-up').css('color', 'red');
+    });
+    
+    $('#goToTop').on('mouseleave', function () {
+      $('.fa-circle-up').css('color', '#FFD43B');
+    });
     
 
     // menu dropdown-open
     $('.dropdown1').click(function (e) { 
       e.preventDefault();
-      $('.dropdown1').toggleClass('active');
-      $('.drop2').slideUp();
+      $('.dropdown1').toggleClass('active').siblings('.dropdown2').removeClass('active2');
       $('.drop1').slideToggle('fast');
-      $('.dropdown2').removeClass('active2');
-      
+      $('.drop2').slideUp();
     });
+
     $('.dropdown2').click(function (e) { 
       e.preventDefault();
-      $('.dropdown2').toggleClass('active2');
-      $('.drop1').slideUp();
+      $('.dropdown2').toggleClass('active2').siblings('.dropdown1').removeClass('active');
       $('.drop2').slideToggle('fast');
-      $('.dropdown1').removeClass('active');
-      
+      $('.drop1').slideUp();
     });
+});
+
+$(document).ready(function () {
+  $(selector).click(function (e) { 
+    e.preventDefault();
+    $(selector).addClass(className);
+    $(selector).toggleClass(className);
+    jqsh
+  });
 });
